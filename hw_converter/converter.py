cf319351dd6f1beaf852f5f0ae9899d11f1d458c
Anton Stambler <anstambler@gmail.com>
2026-07-10 21:26:48 +0300

    hw_converter class

--- hw_converter/converter.py
+++ hw_converter/converter.py
@@ -1,26 +1,21 @@
 from typing import Iterable
 import pandas as pd
 
+class Converter:
 
-def enumerator(values: Iterable[str]) -> dict[str, int]:
-    values = [*values]
-    enum = {str(values[i]): i for i in range(len(values))}
-    return enum
-
-
-def columns_mapper(columns_str: list[str], df: pd.DataFrame) -> dict[str, dict[str, int]]:
-    columns_str = [*columns_str]
-    cm = {columns_str[i]: enumerator([*df[columns_str[i]].unique()]) for i in range(len(columns_str))}
-    return cm
+    def enumerator(self, values: Iterable[str]) -> dict[str, int]:
+        values = [*values]
+        enum = {str(values[i]): i for i in range(len(values))}
+        return enum
 
 
-def convert_x(df: pd.DataFrame, mapper: dict[str, dict[str, int]]) -> pd.DataFrame:
-    conv = {val: [mapper[val][elem] for elem in df[val]] for val in df.columns}
-    return conv
+    def columns_mapper(self, columns_str: list[str], df: pd.DataFrame) -> dict[str, dict[str, int]]:
+        columns_str = [*columns_str]
+        cm = {columns_str[i]: self.enumerator([*df[columns_str[i]].unique()]) for i in range(len(columns_str))}
+        return cm
 
 
-# df = {"Company":['Toyota','Toyota','Hundai', 'Hunday', 'Hunday'], "Model": ['Camry', 'Corolla', 'i10', 'Elantra', 'Kona']}
-# df = pd.DataFrame({"Company":['Toyota','Toyota','Hundai', 'Hundai', 'Hundai'], "Model": ['Camry', 'Corolla', 'i10', 'Elantra', 'Kona']})
-# print(enumerator(df.columns))
-# print(columns_mapper(df.columns, df))
-# print(convert_x(df, columns_mapper(df.columns, df)))
+    def convert_x(self, df: pd.DataFrame, mapper: dict[str, dict[str, int]]) -> pd.DataFrame:
+        conv = {val: [mapper[val][elem] for elem in df[val]] for val in df.columns}
+        return conv
+
