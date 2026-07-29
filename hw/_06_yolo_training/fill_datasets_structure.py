746c6e15af144c8ff56899235864e792429dedf8
Anton Stambler <anstambler@gmail.com>
2026-07-29 22:01:52 +0300

    hw-2.0 Refactor dataset generation script to combine circle and square creation into a single function `make_img_square_circle`

--- hw/_06_yolo_training/fill_datasets_structure.py
+++ hw/_06_yolo_training/fill_datasets_structure.py
@@ -9,33 +9,41 @@ HEIGHT = 256
 NUM_TRAIN = 30
 NUM_VAL = 3
 
-def make_img_circle(x_center, y_center, radius, color, path):
+def make_img_square_circle(path_img, path_label):
+    circles = random.randint(1, 2)
+    squares = random.randint(1, 2)
     bg_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
     img_arr = np.full((HEIGHT, WIDTH, 3), bg_color, np.uint8)
-    cv2.circle(img_arr, (x_center, y_center), radius, color, -1)
-    cv2.imwrite(path, img_arr)
+    labels = []
+    for i in range(circles):
+        x, y, radius, color = random_circle_params()
+        cv2.circle(img_arr, (x, y), radius, color, -1)
+        mlc = make_label_circle(x, y, radius)
+        labels.append(mlc)
+    for i in range(squares):
+        x, y, width, color = random_square_params()
+        cv2.rectangle(img_arr, (x - width // 2, y - width // 2),
+                      (x + width // 2, y + width // 2), color, -1)
+        mls = make_label_square(x, y, width)
+        labels.append(mls)
+    cv2.imwrite(path_img, img_arr)
+    with open(path_label, 'w') as f:
+        f.write('\n'.join(labels))
 
-def make_img_square(x_center, y_center, width, color, path):
-    bg_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    img_arr = np.full((HEIGHT, WIDTH, 3), bg_color, np.uint8)
-    cv2.rectangle(img_arr, (x_center - width // 2, y_center - width // 2), (x_center + width // 2, y_center + width // 2), color, -1)
-    cv2.imwrite(path, img_arr)
 
-def make_label_circle(x_center, y_center, radius, path):
+def make_label_circle(x_center, y_center, radius):
     x = x_center / WIDTH
     y = y_center / HEIGHT
     w = radius * 2 / WIDTH
     h = radius * 2 / HEIGHT
-    with open(path, 'w') as f:
-        f.write(f"0 {x:.6f} {y:.6f} {w:.6f} {h:.6f}")
+    return f"0 {x:.6f} {y:.6f} {w:.6f} {h:.6f}"
 
-def make_label_square(x_center, y_center, width, path):
+def make_label_square(x_center, y_center, width):
     x = x_center / WIDTH
     y = y_center / HEIGHT
     w = width / WIDTH
     h = width / HEIGHT
-    with open(path, 'w') as f:
-        f.write(f"1 {x:.6f} {y:.6f} {w:.6f} {h:.6f}")
+    return f"1 {x:.6f} {y:.6f} {w:.6f} {h:.6f}"
 
 def random_circle_params():
     radius = random.randint(1, min(WIDTH, HEIGHT) // 2)
@@ -55,12 +63,7 @@ def random_square_params():
 
 def generate_dataset(count, images_dir, labels_dir):
     for i in range(count):
-        x, y, radius, color = random_circle_params()
-        make_img_circle(x, y, radius, color, images_dir + f"/img{i}.jpg")
-        make_label_circle(x, y, radius, labels_dir + f"/img{i}.txt")
-        x, y, width, color = random_square_params()
-        make_img_square(x, y, width, color, images_dir + f"/img{i + count}.jpg")
-        make_label_square(x, y, width, labels_dir + f"/img{i + count}.txt")
+        make_img_square_circle(images_dir + f"/img{i}.jpg", labels_dir + f"/img{i}.txt")
 
 generate_dataset(NUM_TRAIN, BASE_DIR + IMAGES_TRAIN, BASE_DIR + LABELS_TRAIN)
 generate_dataset(NUM_VAL, BASE_DIR + IMAGES_VAL, BASE_DIR + LABELS_VAL)
